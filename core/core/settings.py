a860e06a74b4623b91b11d72837a4b505304ca7b
sorushbin <102279809+sorushbin@users.noreply.github.com>
2024-03-07 23:17:51 +0330

    create User model
    -create user model
    -create user manager

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# user manager config
+AUTH_USER_MODEL = 'accounts.User'
