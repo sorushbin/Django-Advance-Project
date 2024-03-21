4d683622f08a0f0c96aed0ef6d3329ba273d32d4
sorushbin <102279809+sorushbin@users.noreply.github.com>
2024-03-21 20:12:03 +0330

    to_representation

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,15 +1,36 @@
 from rest_framework import serializers
 from ...models import Post, Category
 
-class PostSerializers(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'status', 'content', 'author' , 'published_date']
-    
-    
 class CategorySerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Category
         fields = ['id', 'name']
+
+
+class PostSerializers(serializers.ModelSerializer):
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.ReadOnlyField(source='get_absolute_api_url')
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
+    # category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=Category.objects.all())
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'image','status','snippet', 'content', 'absolute_url', 'relative_url', 'author', 'category', 'published_date']
+        
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+    
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet', None)
+            rep.pop('relative_url', None)
+            rep.pop('absolute_url', None)
+        else:
+            rep.pop('content', None)
+        rep['category'] = CategorySerializers(instance.category).data
+        return rep
+    
