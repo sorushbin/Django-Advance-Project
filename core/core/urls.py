e2447b221dcd999d0ca371627b7497f6323835ad
sorushbin <102279809+sorushbin@users.noreply.github.com>
2024-03-23 11:02:52 +0330

    registration serializer
    create RegistrationSerializer for create user and validate

--- core/core/urls.py
+++ core/core/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('blog/', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
